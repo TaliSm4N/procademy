fed8a00570570eb9cd9fdde2019dabc4d207b950
SoMuL <evenine9@gmail.com>
2019-10-17 15:18:56 +0900

    Window project start

---
+++ WindowsProject1/WindowsProject1/GraphWindow.h
@@ -0,0 +1,64 @@
+#pragma once
+#include <windows.h>
+#include <queue>
+
+#define dfMAXCHILD		100
+
+class GraphWindow
+{
+public:
+	enum TYPE
+	{
+		LINE_SINGLE,
+		LINE_MULTI,
+		NUMBER,
+		ONOFF,
+		PIE
+	};
+
+	typedef struct ST_HWNDtoTHIS
+	{
+		HWND			hWnd[dfMAXCHILD];
+		GraphWindow	*pThis[dfMAXCHILD];
+
+	} stHWNDtoTHIS;
+
+	GraphWindow(HINSTANCE hInstance, HWND hWndParent, TYPE enType, int posX, int posY, int iWidth, int iHeight);
+	~GraphWindow();
+
+	static LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+
+	BOOL	InsertData(int iData);
+
+protected:
+
+	//------------------------------------------------------
+	// 윈도우 핸들, this 포인터 매칭 테이블 관리.
+	//------------------------------------------------------
+	BOOL				PutThis(void);
+	static GraphWindow	*GetThis(HWND hWnd);
+
+private:
+
+	//------------------------------------------------------
+	// 부모 윈도우 핸들, 내 윈도우 핸들, 인스턴스 핸들
+	//------------------------------------------------------
+	//------------------------------------------------------
+	// 윈도우 위치,크기,색상, 그래프 타입 등.. 자료
+	//------------------------------------------------------
+	TYPE		_enGraphType;
+
+	//------------------------------------------------------
+	// 더블 버퍼링용 메모리 DC, 메모리 비트맵
+	//------------------------------------------------------
+	HDC		_hMemDC;
+	HBITMAP		_hBitmap;
+
+	//------------------------------------------------------
+	// 데이터
+	//------------------------------------------------------
+	std::queue<int> DataQueue;
+
+		// static 맴버 함수의 프로시저에서 This 포인터를 찾기 위한
+		// HWND + Class Ptr 의 테이블
+};
